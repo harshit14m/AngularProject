d65daf23a4e702f1b683fdb32d018afc5c1202fa
Harshit <harshit.14m@gmail.com>
2024-08-01 02:22:51 +0530

    rxjs learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginformComponent } from './component/loginform/loginform.component';
 import { LoginPageComponent } from './container/login-page/login-page.component';
+import { RxjsLearningComponent } from './rxjs-learning/rxjs-learning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginformComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    RxjsLearningComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-learning',
+  templateUrl: './rxjs-learning.component.html',
+  styleUrls: ['./rxjs-learning.component.css']
+})
+export class RxjsLearningComponent {
+
+}
